32175d06b169359ce0169ead2ab957a0997a9005
ChethaNavid <164472640+ChethaNavid@users.noreply.github.com>
2025-06-22 22:58:52 +0700

    Home page

--- frontend/grocery_store/src/pages/Home/Home.jsx
+++ frontend/grocery_store/src/pages/Home/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import NavBar from '../../components/NavBar/NavBar'
+import AdsCard from '../../components/Card/AdsCard'
+import ProductCard from '../../components/Card/ProductCard'
+import FooterCard from '../../components/Card/FooterCard'
+import CategoriesNavbar from '../../components/NavBar/CategoriesNavbar'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='pt-[120px] overflow-y-hidden'>
+      <NavBar />
+      <CategoriesNavbar />
+      <AdsCard />
+      <p className='m-4 text-xl font-bold'>Featurd Products</p>
+      <ProductCard />
+      <p className='m-4 text-xl font-bold'>Popular Products</p>
+      <ProductCard />
+      <FooterCard />
+    </div>
   )
 }
 
