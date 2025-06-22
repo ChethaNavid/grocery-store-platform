3f7b97ecff3a1e5534b95b89695bfb4c38dec7cc
ChethaNavid <164472640+ChethaNavid@users.noreply.github.com>
2025-06-22 22:57:16 +0700

    SignUp page

--- frontend/grocery_store/src/pages/SignUp/SignUp.jsx
+++ frontend/grocery_store/src/pages/SignUp/SignUp.jsx
@@ -1,8 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
+import PasswordInput from '../../components/Input/PasswordInput';
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [phoneNum, setPhoneNum] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSignUp = async(e) => {
+    e.preventDefault();
+    if(!username) {
+        setError("Please enter your username");
+        return;
+    } 
+
+    if(!validateEmail(email)) {
+        setError("Please enter a valid email address.");
+        return;
+    }
+
+    if(!password) {
+        setError("Please enter the password");
+        return;
+    }
+    setError("");
+
+    // Sign Up API Call
+  }
+
   return (
-    <div>SignUp</div>
+    <>
+      <div className='flex justify-center items-center mt-20'>
+          <div className='w-96 border rounded bg-white px-7 py-10'>
+              <form onSubmit={() => {}}>
+                  <h4 className='text-2xl mb-7'>Sign Up</h4>
+                  <input 
+                      className='input-box' 
+                      type='text' 
+                      placeholder='Username'
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                  />
+                  <input 
+                      type='text' 
+                      placeholder='Email' 
+                      className='input-box'
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <input 
+                      type='text' 
+                      placeholder='Phone Number' 
+                      className='input-box'
+                      value={phoneNum}
+                      onChange={(e) => setPhoneNum(e.target.value)}
+                  />
+                  <PasswordInput 
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                  />
+
+                  {error && <p className='text-red-500 text-xs pb-1'>{error}</p>}
+
+                  <button type='submit' className='auth-btn'>Sign Up</button>
+
+                  <p className='text-sm text-center mt-4'>
+                      Already have an account? {""}
+                      <Link to="/login" className='font-medium text-primary underline'>
+                          Login
+                      </Link>
+                  </p>
+              </form>
+
+          </div>
+      </div>
+    </>
   )
 }
 
