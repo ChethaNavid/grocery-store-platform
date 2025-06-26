ba001a1037eba3d46e6bcaccb74c73d8509e0d9a
ChethaNavid <164472640+ChethaNavid@users.noreply.github.com>
2025-06-26 14:51:27 +0700

    Navigation updated

--- frontend/grocery_store/src/components/NavBar/SearchBar.jsx
+++ frontend/grocery_store/src/components/NavBar/SearchBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { FaMagnifyingGlass } from "react-icons/fa6";
 import { IoMdClose } from "react-icons/io";
 
-const SearchBar = ({ value, onChange }) => {
+const SearchBar = ({ value, onChange, handleSearch, onClearSearch}) => {
   return (
     <div className='w-[60%] border flex items-center gap-2.5 border-[#ccc] rounded-lg bg-white p-2'>
         <FaMagnifyingGlass 
@@ -15,7 +15,11 @@ const SearchBar = ({ value, onChange }) => {
             onChange={onChange}
             className='w-full outline-none p-2 bg-transparent text-sm'
         />
-        <button className='primary-btn text-sm'>Search</button>
+        {value && <IoMdClose 
+            className='cursor-pointer mr-3'
+            onClick={onClearSearch}
+        />}
+        <button className='primary-btn text-sm'onClick={handleSearch}>Search</button>
     </div>
   )
 }
