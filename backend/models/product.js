91ddda5b27384a626583fa6c74e2859ff461dd44
ChethaNavid <164472640+ChethaNavid@users.noreply.github.com>
2025-06-27 10:23:55 +0700

    setup database with sequelize

---
+++ backend/models/product.js
@@ -0,0 +1,31 @@
+import sequelize from "../database/database.js";
+import { DataTypes } from "sequelize";
+
+export const Product = sequelize.define("Product", {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    inStock: {
+        type: DataTypes.BOOLEAN,
+    },
+    price: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    imgURL: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    timestamps: false
+})
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config';
+import { sequelize } from '../models/index.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -9,6 +11,13 @@ app.use(cors({
     origin: "*",
 }))
 
-app.listen(PORT, () => {
+try {
+  const result = await sequelize.sync({alter: true});
+  console.log(result);
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+app.listen(process.env.PORT, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
 })
