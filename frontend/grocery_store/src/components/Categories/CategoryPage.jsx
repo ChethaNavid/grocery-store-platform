c493b83340692ce2053ec0a6c57466cee3f8a78e
ChethaNavid <164472640+ChethaNavid@users.noreply.github.com>
2025-07-03 16:13:06 +0700

    displaying products in the category page

--- frontend/grocery_store/src/components/Categories/CategoryPage.jsx
+++ frontend/grocery_store/src/components/Categories/CategoryPage.jsx
@@ -1,10 +1,72 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+import NavBar from '../../components/NavBar/NavBar'
+import AdsCard from '../../components/Card/AdsCard'
+import ProductCard from '../../components/Card/ProductCard'
+import FooterCard from '../../components/Card/FooterCard'
+import CategoriesNavbar from '../../components/NavBar/CategoriesNavbar'
+import { useNavigate } from 'react-router-dom'
+import axiosInstance from '../../utils/axiosInstance'
 
 const CategoryPage = () => {
   const { categoryName } = useParams();
+
+  const [allProduct, setAllProduct] = useState([]);
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+
+  const onLogout = () => {
+    localStorage.clear();
+    navigate('/login');
+    setisLoggedIn(false);
+  }
+
+  // Get All Products
+  const getProductFromCategory = async () => {
+      try {
+          const response = await axiosInstance.get(`/category/${categoryName}`);
+          if(response.data && response.data.products) {
+              setAllProduct(response.data.products);
+          }
+      } catch (error) {
+          console.log("Unexpected error occurred.");
+      }
+  }
+
+  const handleAddButton = () => {
+
+  }
+
+  useEffect(() => {
+    getProductFromCategory();
+    return () => {}
+  }, [categoryName])
+
   return (
-    <div>CategoryPage</div>
+    <div className='pt-[120px]'>
+      <NavBar isLoggedIn={isLoggedIn} onLogout={onLogout} onSearchNote={() => {}} handleClearSearch={() => {}} />
+
+      <CategoriesNavbar />
+
+      <p className='m-4 text-xl font-bold'>{categoryName.charAt(0).toUpperCase() + categoryName.slice(1)} Products</p>
+
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 px-4 pb-8'>
+        {allProduct.map((items) => {
+          return (
+            <ProductCard 
+              imgURL={items.imageUrl}
+              category={items.Category?.name}
+              productName={items.name}
+              price={`$${items.price}`}
+              handleAddButton={() => {handleAddButton(items)}}
+            />
+          )
+        })}
+      </div>
+
+      <FooterCard />
+
+    </div>
   )
 }
 
--- frontend/grocery_store/src/components/NavBar/CategoriesNavbar.jsx
+++ frontend/grocery_store/src/components/NavBar/CategoriesNavbar.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 const CategoriesNavbar = () => {
   return (
     <nav className="fixed top-[69px] w-full left-0 z-99 text-sm text-slate-600 border border-[#ccc] flex gap-[30px] bg-[#F9FAFB] px-2.5 py-3.5">
-      <Link to="/category/fruit">Fruit</Link>
+      <Link to="/category/fruits">Fruits</Link>
       <Link to="/category/vegetables">Vegetables</Link>
-      <Link to="/category/diary">Diary</Link>
+      <Link to="/category/dairy">Dairy</Link>
       <Link to="/category/meat">Meat</Link>
       <Link to="/category/bakery">Bakery</Link>
       <Link to="/category/beverages">Beverages</Link>
