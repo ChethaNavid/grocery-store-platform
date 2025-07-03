5c5d994beefe05138f249aa2fa70c8ffb180a968
ChethaNavid <164472640+ChethaNavid@users.noreply.github.com>
2025-07-03 16:08:56 +0700

    make change to the models

--- backend/models/order.js
+++ backend/models/order.js
@@ -3,12 +3,16 @@ import { DataTypes } from "sequelize";
 
 export const Order = sequelize.define("Order", {
     totalAmount: {
-        type: DataTypes.FLOAT,
+        type: DataTypes.INTEGER,
         allowNull: false,
         validate: {min: 0}
     },
+    totalPrice: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+    },
     status: {
-        type: DataTypes.ENUM("pending", "dilivered", "canceled"),
+        type: DataTypes.ENUM("pending", "delivered", "cancelled"),
         defaultValue: "pending"
     },
     address: {
