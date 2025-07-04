2ef659b8e0abbdfbf31aa6b7df8f74500aa54cb4
ChethaNavid <164472640+ChethaNavid@users.noreply.github.com>
2025-07-04 09:12:36 +0700

    update admin page

--- frontend/grocery_store/src/components/Admin/AdminHomePage.jsx
+++ frontend/grocery_store/src/components/Admin/AdminHomePage.jsx
@@ -1,15 +1,57 @@
 import React from 'react'
 import NavBarAdmin from '../../components/Admin/NavBarAdmin'
-import ProductListAdmin from '../../components/Admin/ProductListAdmin'
+import ProductCardAdmin from './ProductCardAdmin'
 import CategoriesNavbarAdmin from '../../components/Admin/CategoriesNavBarAdmin'
+import axiosInstance from '../../utils/axiosInstance'
+import { useState, useEffect } from 'react'
+import { useParams } from "react-router-dom";
 
 const AdminHome = () => {
+  const { categoryName } = useParams();
+
+  const [allProduct, setAllProduct] = useState([]);
+
+  // Get All Products
+  const getProduct = async () => {
+      try {
+          const response = categoryName 
+          ? await axiosInstance.get(`/category/${categoryName}`)
+          : await axiosInstance.get('/admin/products');
+
+          if(response.data && response.data.products) {
+              setAllProduct(response.data.products);
+          }
+      } catch (error) {
+          console.log("Unexpected error occurred.");
+      }
+  }
+
+  useEffect(() => {
+    getProduct();
+    return () => {}
+  }, [categoryName])
+
   return (
-    <div className='pt-[120px] overflow-y-hidden'>
+    <div className='pt-[150px]'>
       <NavBarAdmin />
       <CategoriesNavbarAdmin />
-      <h1 className="text-2xl font-bold my-6">Admin Product Management</h1>
-      <ProductListAdmin />
+
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 px-5 pb-8'>
+        {allProduct.map((items) => {
+          return (
+            <ProductCardAdmin
+              key={items.id}
+              imgURL={items.imageUrl}
+              category={items.Category?.name}
+              productName={items.name}
+              price={`$${items.price}`}
+              inStock={items.inStock}
+              onEdit={() => {}}
+              onDelete={() => {}}
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
