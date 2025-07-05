e07d25365eb6b3e9e6f8867e2d2156cfbcec1aa6
ChethaNavid <164472640+ChethaNavid@users.noreply.github.com>
2025-07-05 07:44:06 +0700

    product controller ready for integration

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { getAllProduct, getPopularProduct, getProductFromCategory } from "../controllers/productController.js";
+import { getFeatureProduct, getPopularProduct, getProductFromCategory } from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
-productRouter.get('/product', getAllProduct);
+productRouter.get('/product', getFeatureProduct);
 productRouter.get('/popular-product', getPopularProduct);
 productRouter.get('/category/:categoryName', getProductFromCategory);
 
