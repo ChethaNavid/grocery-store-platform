320c6cb57b17075dddecb8f24546fd26b643084c
ChethaNavid <164472640+ChethaNavid@users.noreply.github.com>
2025-07-05 07:41:49 +0700

    admin controller ready for integration

--- backend/routes/adminRouter.js
+++ backend/routes/adminRouter.js
@@ -1,10 +1,15 @@
 import express from 'express'
-import { addProduct, editProduct, deleteProduct } from '../controllers/adminController.js'
+import { addProduct, editProduct, deleteProduct, getAllProduct } from '../controllers/adminController.js'
+import multer from 'multer';
 
 const adminRouter = express.Router();
 
-adminRouter.post('/create-product', addProduct);
-adminRouter.put('/edit-product/:id', editProduct);
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+
+adminRouter.get('/products', getAllProduct);
+adminRouter.post('/add-product', upload.single('image'), addProduct);
+adminRouter.put('/edit-product/:id', upload.single('image'), editProduct);
 adminRouter.delete('/delete-product/:id', deleteProduct);
 
 export default adminRouter;
