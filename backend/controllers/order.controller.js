d4e7cdeca1a80fb0f2b1cfb9eae7a8be349950ac
ChethaNavid <164472640+ChethaNavid@users.noreply.github.com>
2025-07-14 13:07:48 +0700

    Update: update order controller and route

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -1,5 +1,7 @@
 import { Order } from "../models/order.js";
 import { OrderDetail } from "../models/orderDetail.js";
+import { Payment } from "../models/payment.js";
+import { v4 as uuidv4 } from 'uuid';
 
 // POST/create-order
 export const createOrder = async (req, res) => {
@@ -27,6 +29,23 @@ export const createOrderDetail = async (req, res) => {
             quantity, pricePerUnit, productId, orderId
         })
         return res.status(201).json({ error: false, orderDetail, message: "Order detail created successfully" });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: true, message: error });
+    }
+}
+
+// POST/create-payment
+export const createPayment = async (req, res) => {
+    const { method, amount, orderId } = req.body;
+
+    try {
+        const transactionRef = uuidv4();
+
+        const createPayment = await Payment.create({
+            method, amount, transactionRef, orderId
+        })
+        return res.status(201).json({ error: true, createPayment, message: "Payment created successfully" });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ error: true, message: error });
